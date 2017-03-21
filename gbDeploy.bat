82203cc550b6b0767385451cfd477219bd6a7a78
weizhou <toweizhou@gmail.com>
2017-03-21 12:00:08 +0800

    new content

---
+++ gbDeploy.bat
@@ -0,0 +1,5 @@
+d:
+cd D:\JavaWork\Tomcat7\webapps\gitbook
+del /s/q intro
+rd /s/q intro
+copy "C:\Documents and Settings\Administrator\.jenkins\workspace\manual\intro" D:\JavaWork\Tomcat7\webapps\gitbook
