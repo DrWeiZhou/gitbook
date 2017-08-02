94c0e54721fd0d1da4cfef2c4f39d12942d104c1
WeiZhou <toweizhou@gmail.com>
2017-08-02 05:57:20 +0000

    gitbook build finished

--- gbbuild.sh
+++ gbbuild.sh
@@ -2,9 +2,11 @@
 #cd ~/weizhougitbooks/gitbook
 git pull
 #cd ..
-#gitbook build . ./intro
+gitbook build . ./intro
 gitbook pdf
 #cd gitbook
 git add -A
 git commit -m 'gitbook build finished'
 git push
+
+
