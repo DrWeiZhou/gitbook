3466e47e9ae14207fc7e0b9b9380662431e75c51
WeiZhou <toweizhou@gmail.com>
2017-08-02 10:34:48 +0000

    gitbook build finished

--- intro/gbDeploy.bat
+++
@@ -1,6 +0,0 @@
-d:
-cd D:\JavaWork\Tomcat7\webapps\gitbook
-del /q *.*
-del /s/q intro
-rd /s/q intro
-xcopy /s /i /y "C:\Documents and Settings\Administrator\.jenkins\workspace\manual\intro" "D:\JavaWork\Tomcat7\webapps\gitbook\intro"
--- intro/gbUpdate.bat
+++
@@ -1,6 +0,0 @@
-git pull
-git add -A
-git commit -m "new content"
-git push
-docker run --rm weizhou/gitbookserver1:notpull_gitbook /root/weizhougitbooks/gitbook/gbbuild.sh
-git pull
