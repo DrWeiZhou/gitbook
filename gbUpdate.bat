450b62fcf6b5b8feee17eb6ec1978bf31505d293
weizhou <toweizhou@gmail.com>
2017-08-02 14:01:24 +0800

    new content

--- gbUpdate.bat
+++ gbUpdate.bat
@@ -2,4 +2,4 @@ git pull
 git add -A
 git commit -m "new content"
 git push
-#docker run --rm weizhou/gitbookserver1:notpull_gitbook /root/weizhougitbooks/gitbook/gbbuild.sh
+docker run --rm weizhou/gitbookserver1:notpull_gitbook /root/weizhougitbooks/gitbook/gbbuild.sh
